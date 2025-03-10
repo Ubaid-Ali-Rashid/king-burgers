57d1b6f73c66dd1bc39c9192bf470449fc6e16d1
Ubaid Ali <ubaidrashid256@gmail.com>
2025-03-10 09:36:51 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,122 @@
+
+
+function show(){
+    $('.nav').css({"display":"none"});
+    $('.navmenu').css({"display":"block"});
+    $('.navbar').css({'background':"none"});
+}
+
+$('#a1').mouseenter(function () { 
+    $(this).css({
+        "color":"red",
+        "text-decoration": "underline",
+    });
+    $('#a2').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+    $('#a3').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+    $('#a4').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+    $('#a5').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+});
+
+$('#a2').mouseenter(function () { 
+    $(this).css({
+        "color":"red",
+        "text-decoration": "underline",
+    });
+    $('#a1').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+    $('#a3').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+    $('#a4').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+    $('#a5').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+});
+
+$('#a3').mouseenter(function () { 
+    $(this).css({
+        "color":"red",
+        "text-decoration": "underline",
+    });
+    $('#a2').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+    $('#a1').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+    $('#a4').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+    $('#a5').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+});
+
+$('#a4').mouseenter(function () { 
+    $(this).css({
+        "color":"red",
+        "text-decoration": "underline",
+    });
+    $('#a2').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+    $('#a3').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+    $('#a1').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+    $('#a5').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+});
+
+$('#a5').mouseenter(function () { 
+    $(this).css({
+        "color":"red",
+        "text-decoration": "underline",
+    });
+    $('#a2').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+    $('#a3').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+    $('#a4').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+    $('#a1').css({
+        "text-decoration":"none",
+        "color":"blue"
+    })
+});
